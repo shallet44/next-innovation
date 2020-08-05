2f57bf0b119e9a2652ac410070dd30d0118ec6d1
Jong Hun <shallet44@naver.com>
2020-08-05 12:08:51 +0900

    python 100jun problem

--- python/100jun/p.py
+++ python/100jun/p.py
@@ -1,1 +1,13 @@
-print('test')
+m = int(input()); n = int(input())
+A = []
+B = []
+for i in range(1, 101):
+    a = i * i
+    if m <= a <= n:
+        A.append(a)
+if A == B:
+    print(-1)
+else:
+    print(sum(A))
+    print(min(A))
+
